755cea9bcd1588051ff3f318171f0bf59f13be6b
dhumalkomal <107318301+dhumalkomal@users.noreply.github.com>
2023-12-29 15:26:26 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+          <h1>Todolist App</h1>
+        </div>
+      )
+}
+
+export default Header;
